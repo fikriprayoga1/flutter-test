783833cffc0d48edbb34da23511e19ea0970322e
Fikri Prayoga <fikri@extrainteger.com>
2023-01-18 11:30:08 +0700

    Integration testing added

--- flutter_testing/integration_test/app_test.dart
+++ flutter_testing/integration_test/app_test.dart
@@ -7,9 +7,11 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
 import 'package:flutter_testing/main.dart';
 
 void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
